16d7df745b7e369484fa3e7849a02a5eb8ae1c2d
hieu <hieuvnautotech@gmail.com>
2022-11-16 09:19:56 +0700

    gán cứng ui cho native selector tự làm

--- tl/src/components/CountrySelector/index.js
+++ tl/src/components/CountrySelector/index.js
@@ -1,30 +1,56 @@
-import React from 'react'
-import InputLabel from '@material-ui/core/InputLabel';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import NativeSelect from '@material-ui/core/NativeSelect';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import NativeSelect from "@material-ui/core/NativeSelect";
+import { makeStyles } from "@material-ui/core/styles";
+import Select from "@material-ui/core/Select";
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 export default function CountrySelector() {
+  const classes = useStyles();
   return (
-    
-
-    <FormControl>
+    <div>
+      <FormControl className={classes.formControl}>
         <InputLabel shrink htmlFor="country-selector">
           Quốc Gia
         </InputLabel>
         <NativeSelect
-          value={value}
-          onChange={handleOnChange}
-          inputProps={{
-            name:'country',
-            id:'country-selector',
-          }}
-        >
-          
-        </NativeSelect>
-       <FormHelperText>Lựa chọn quốc gia</FormHelperText>
-
+          value={{}}
+                onChange={{}}
+                inputProps={{
+                  name:'country',
+                  id:'country-selector',
+                }}
+        ></NativeSelect>
       </FormControl>
-  )
+    </div>
+
+    // <FormControl>
+    //     <InputLabel shrink htmlFor="country-selector">
+    //       Quốc Gia
+    //     </InputLabel>
+    //     <NativeSelect
+    //       value={value}
+    //       onChange={handleOnChange}
+    //       inputProps={{
+    //         name:'country',
+    //         id:'country-selector',
+    //       }}
+    //     >
+
+    //     </NativeSelect>
+    //    <FormHelperText>Lựa chọn quốc gia</FormHelperText>
+
+    //   </FormControl>
+  );
 }
